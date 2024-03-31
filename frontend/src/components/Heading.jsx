e509b9e00d9c4ea8b5a1d5f33a3dc5a9699d3cad
fyzanshaik <fyzan.shaik@gmail.com>
2024-03-31 18:51:57 +0530

    done

--- frontend/src/components/Heading.jsx
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line react/prop-types
-export function Heading({text}){
-    return (
-        <div className="font-bold text-4xl pt-2">
-            {text}
-        </div>
-    )
-}
--- frontend/src/components/InputBox.jsx
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line react/prop-types
-export function InputBox({ label, placeholder ,onChange}) {
-    return (
-        <div className="py-2 text-left font-medium">
-            <label className="">{label}</label><br></br>
-            <input onChange={onChange} className="text-sm rounded-lg border border-grey-300  block w-full p-2.5" type="text" placeholder={placeholder} />
-        </div>
-    )
-}
